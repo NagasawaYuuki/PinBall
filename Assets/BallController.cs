8e104f4bc379b11d3fc7e0759cbf6fecb112ec51
NagasawaYuuki <69589388+NagasawaYuuki@users.noreply.github.com>
2020-10-03 14:01:38 +0900

    PinBall コミット

---
+++ Assets/BallController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BallController : MonoBehaviour
+{
+    private float visiblePosZ = -6.5f;
+    private GameObject gameoverText;
+    GameObject director;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameoverText = GameObject.Find("GameOverText");
+        this.director = GameObject.Find("GameDirector");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log()
+         
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            this.director.GetComponent<GameDirector>().GetSmallStar();
+   
+        }
+        if (other.gameObject.tag == "LargeStarTag")
+        {
+            this.director.GetComponent<GameDirector>().GetLargeStar();
+        }
+        if (other.gameObject.tag == "SmallCloudTag")
+        {
+            this.director.GetComponent<GameDirector>().GetSmallCloud();
+        }
+        if (other.gameObject.tag == "LargeCloudTag")
+        {
+            this.director.GetComponent<GameDirector>().GetLargeCloud();
+        }
+    }
+}
---
+++ Assets/GameDirector.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameDirector : MonoBehaviour
+{
+    private GameObject scoreText;
+    int score = 0;
+
+    public void GetSmallStar()
+    {
+        this.score += 50;
+    }
+    public void GetLargeStar()
+    {
+        this.score += 150;
+    }
+    public void GetSmallCloud()
+    {
+        this.score += 100;
+    }
+    public void GetLargeCloud()
+    {
+        this.score += 200;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.scoreText = GameObject.Find("ScoreText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.scoreText.GetComponent<Text>().text = "score : " + this.score;
+
+    }
+}
